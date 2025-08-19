374ad95040b4dc3005065061db6ee0b0e3c7e680
Abhi <abhinav118@gmail.com>
2025-08-19 16:36:10 -0400

    Add components/ActionButton.tsx

---
+++ components/ActionButton.tsx
@@ -0,0 +1,13 @@
+interface ActionButtonProps {
+  padding?: any
+  rounded?: any
+  font?: any
+}
+
+export default function ActionButton({ padding, rounded, font }: ActionButtonProps) {
+  return (
+    <div className="">
+      {/* ActionButton component content */}
+    </div>
+  )
+}
