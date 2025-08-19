3831ae23ad3094d2b3a27e536b5ff46e06cfa0b0
Abhi <abhinav118@gmail.com>
2025-08-19 16:36:10 -0400

    Add components/SearchInput.tsx

---
+++ components/SearchInput.tsx
@@ -0,0 +1,13 @@
+interface SearchInputProps {
+  placeholder?: any
+  icon?: any
+  clearable?: any
+}
+
+export default function SearchInput({ placeholder, icon, clearable }: SearchInputProps) {
+  return (
+    <div className="">
+      {/* SearchInput component content */}
+    </div>
+  )
+}
