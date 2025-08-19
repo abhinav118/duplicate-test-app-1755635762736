74d8e9490d90256b9e1b8bd2b738d3ee18777ed9
Abhi <abhinav118@gmail.com>
2025-08-19 16:36:10 -0400

    Add components/DataTable.tsx

---
+++ components/DataTable.tsx
@@ -0,0 +1,13 @@
+interface DataTableProps {
+  striped?: any
+  hover?: any
+  bordered?: any
+}
+
+export default function DataTable({ striped, hover, bordered }: DataTableProps) {
+  return (
+    <div className="">
+      {/* DataTable component content */}
+    </div>
+  )
+}
