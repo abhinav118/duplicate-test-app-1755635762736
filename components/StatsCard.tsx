878ca1ee1190258e4cc9adc1176d742e0b890375
Abhi <abhinav118@gmail.com>
2025-08-19 16:36:09 -0400

    Add components/StatsCard.tsx

---
+++ components/StatsCard.tsx
@@ -0,0 +1,13 @@
+interface StatsCardProps {
+  padding?: any
+  rounded?: any
+  shadow?: any
+}
+
+export default function StatsCard({ padding, rounded, shadow }: StatsCardProps) {
+  return (
+    <div className="">
+      {/* StatsCard component content */}
+    </div>
+  )
+}
